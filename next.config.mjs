c166d8e01eea608680092861121813df590fb938
Alec Ortega Cisneros <sistemas_alec@segurosdelascalifornias.local>
2024-09-23 10:01:37 -0700

    feature/Add an assetPrefix

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
+  assetPrefix:
+    process.env.NODE_ENV === "production" ? "/alecfut07.github.io/" : "",
 };
 
 export default nextConfig;
