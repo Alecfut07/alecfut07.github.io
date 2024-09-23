053ae96dac6010c84375ec34b9c24b8365551e90
Alec Ortega Cisneros <sistemas_alec@segurosdelascalifornias.local>
2024-09-23 16:04:10 -0700

    bugfix/Remove assetPrefix on next configuration

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
-  assetPrefix:
-    process.env.NODE_ENV === "production" ? "/alecfut07.github.io/" : "",
 };
 
 export default nextConfig;
